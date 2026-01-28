802169c12205fdb417025dad73eeab9418ae9c35
maklean <yoelmakaya@gmail.com>
2026-01-28 00:58:22 -0500

    impl: add fetch_mem_stats() to get memory stats (total, free, and available memory)

--- core/telemetry.c
+++ core/telemetry.c
@@ -4,12 +4,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <limits.h>
 
 // Puts `buffer_size`-1 characters from the file at `file_path` into `buffer`.
-static int read_line_into_buffer(const char *file_path, char *buffer, size_t buffer_size);
+static int read_lines_into_buffer(const char *file_path, char *buffer, size_t buffer_size, size_t line_count);
 
 // Calculates the CPU Usage over `time_ms` ms in time.
 int calculate_cpu_usage(double *cpu_usage, int time_ms);
+
+// Fetches the memory stats and injects them into the given pointers.
+int fetch_mem_stats(uint64_t *total_mem_kb, uint64_t *free_mem_kb, uint64_t *available_mem_kb);
 
 // Injects the jiffies into the `jiffies` array.
 static int inject_jiffies(long int jiffies[2]);
@@ -23,7 +28,7 @@ int collect_vitals(SystemVitals *vitals) {
 int get_kernel_version(char *buffer, size_t size) {
     if(!buffer) return ERR_INVALID_ARG_PTR;
 
-    int res = read_line_into_buffer(KERNEL_VERSION_PATH, buffer, size);
+    int res = read_lines_into_buffer(KERNEL_VERSION_PATH, buffer, size, 1);
     if(res != C_OK) return res;
 
     // remove newline
@@ -35,7 +40,7 @@ int get_kernel_version(char *buffer, size_t size) {
     return C_OK;
 }
 
-int read_line_into_buffer(const char *file_path, char *buffer, size_t buffer_size) {
+int read_lines_into_buffer(const char *file_path, char *buffer, size_t buffer_size, size_t line_count) {
     if(!file_path) return ERR_INVALID_FILE_PATH_PTR;
     if(!buffer) return ERR_INVALID_ARG_PTR;
 
@@ -43,8 +48,15 @@ int read_line_into_buffer(const char *file_path, char *buffer, size_t buffer_siz
     FILE *fPtr = fopen(file_path, "r");
     if(!fPtr) return ERR_FAILED_TO_OPEN_FILE;
 
-    // read 'buffer_size'-1 characters into bufer
-    if(!fgets(buffer, buffer_size, fPtr)) return ERR_FAILED_TO_READ_INTO_BUFFER;
+    char tmp_buffer[READ_PROC_FILE_BUFFER];
+
+    // read 'line_count' lines from file
+    while(fgets(tmp_buffer, READ_PROC_FILE_BUFFER, fPtr) && line_count) {
+        strcat(buffer, tmp_buffer);
+        line_count--;
+    }
+
+    if(line_count != 0) return ERR_FAILED_TO_READ_INTO_BUFFER;
 
     fclose(fPtr);
 
@@ -83,8 +95,8 @@ int inject_jiffies(long int jiffies[2]) {
 
     int res;
 
-    char buffer[CPU_USAGE_BUFFER];
-    if((res = read_line_into_buffer("/proc/stat", buffer, CPU_USAGE_BUFFER)) != 0) {
+    char buffer[READ_PROC_FILE_BUFFER] = {0};
+    if((res = read_lines_into_buffer("/proc/stat", buffer, READ_PROC_FILE_BUFFER, 1)) != C_OK) {
         return res;
     }
 
@@ -109,6 +121,53 @@ int inject_jiffies(long int jiffies[2]) {
 
     jiffies[0] = total;
     jiffies[1] = work;
+
+    return C_OK;
+}
+
+int fetch_mem_stats(uint64_t *total_mem_kb, uint64_t *free_mem_kb, uint64_t *available_mem_kb) {
+    if(!total_mem_kb || !free_mem_kb || !available_mem_kb) return ERR_INVALID_ARG_PTR;
+
+    int res;
+    char buffer[READ_PROC_FILE_BUFFER] = {0};
+    
+    if((res = read_lines_into_buffer("/proc/meminfo", buffer, READ_PROC_FILE_BUFFER, 3)) != C_OK) {
+        return res;
+    }
+
+    char *save_lines;
+    char *lines = strtok_r(buffer, "\n", &save_lines);
+    
+    char *dup; // TODO: is there a better alternative than strdup()? (b/c strtok'ing for two diff. things just breaks everything)
+    char *dup_sep;
+    char *save_dup;
+
+    size_t reading = 0; // 0 = memtotal, 1 = memfree, 2 = memavailable
+
+    uint64_t val;
+    char *end;
+
+    while(lines != NULL) {
+        dup = strdup(lines);
+        if(!dup) return ERR_FAILED_TO_DUPLICATE_PROC_LINE;
+
+        dup_sep = strtok_r(dup, ":", &save_dup);
+        dup_sep = strtok_r(NULL, ":", &save_dup); // skip key
+
+        val = strtoull(dup_sep, &end, 10);
+
+        if(reading == 0) {
+            *total_mem_kb = val;
+        } else if(reading == 1) {
+            *free_mem_kb = val;
+        } else {
+            *available_mem_kb = val;
+        }
+
+        free(dup);
+        lines = strtok_r(NULL, "\n", &save_lines);
+        reading++;
+    }
 
     return C_OK;
 }
--- core/telemetry.h
+++ core/telemetry.h
@@ -11,14 +11,17 @@
 #define ERR_INVALID_FILE_PATH_PTR -3
 #define ERR_FAILED_TO_OPEN_FILE -4
 #define ERR_FAILED_TO_READ_INTO_BUFFER -5
+#define ERR_FAILED_TO_DUPLICATE_PROC_LINE -6
+#define ERR_FAILED_TO_PARSE_MEMORY_AMOUNT -7
 
 #define KERNEL_VERSION_PATH "/proc/version"
-#define CPU_USAGE_BUFFER 256
+#define READ_PROC_FILE_BUFFER 256
 
 typedef struct {
     double cpu_usage_percentage;
-    uint64_t total_mem_kb;
-    uint64_t free_mem_kb;
+    uint64_t total_mem_kb; // total memory
+    uint64_t free_mem_kb; // amount of physical ram left unused by the system
+    uint64_t available_mem_kb; // estimate of how much memory is available for stating new applications without swapping
     uint64_t uptime_seconds;
     uint16_t network_interface_count;
 } SystemVitals;
